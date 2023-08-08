6773519a72102f2bfb4c28119069c640dbf71695
seungwoo <seungwoolee99@gmail.com>
2023-08-08 15:19:07 +0900

    add warmingCenter

--- data-raw/서울특별시_무더위쉼터/서울시무더위쉼터.R
+++ data-raw/서울특별시_무더위쉼터/서울시무더위쉼터.R
@@ -3,7 +3,7 @@ library(tidyverse)
 setwd("../서울특별시_무더위쉼터/")
 
 df <- read.csv('./서울시 무더위쉼터.csv', fileEncoding = 'utf-8', header = T)
-
+View(df)
 coolCenter <- df %>% 
   select(c(-1:-4,-15,-16))
 
