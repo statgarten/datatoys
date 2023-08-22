c420075813817443a39693f534626d4db92a248f
seungwoo <seungwoolee99@gmail.com>
2023-08-22 18:18:07 +0900

    add seoulRestroom

---
+++ data-raw/서울특별시_공중화장실위치정보/공중화장실위치정보.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+library(lubridate)
+
+df <- read.csv('서울시 공중화장실 위치정보 (좌표계_ WGS1984).csv', fileEncoding = 'utf-8', header = T)
+
+
+glimpse(seoulRestroom)
+
+seoulRestroom <- df %>% 
+  select(c(2:3,9:10)) %>% 
+  rename(
+    `주소(구)` = `구명`,
+    `주소(동)` = `법정동명`
+  )
+
+usethis::use_data(seoulRestroom, overwrite = TRUE)
