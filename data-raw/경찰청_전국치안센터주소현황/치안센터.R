1262dda3678847bfd1814dc5823e91b10ade9d55
seungwoo <seungwoolee99@gmail.com>
2023-08-24 15:05:49 +0900

    add policeBox

---
+++ data-raw/경찰청_전국치안센터주소현황/치안센터.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+library(lubridate)
+
+df <- read.csv('경찰청_전국 치안센터 주소 현황_20221231.csv', fileEncoding = 'utf-8', header = T)
+
+policeBox <- df %>% 
+  select(-1) %>% 
+  rename(
+    지역명 = 경찰서
+  )
+
+usethis::use_data(policeBox, overwrite = TRUE)
