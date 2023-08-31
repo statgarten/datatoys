6e5139110bd8da19cdce01936e01533c4706a18d
seungwoo <seungwoolee99@gmail.com>
2023-08-31 22:15:48 +0900

    add seoulGraute

---
+++ data-raw/서울특별시_대학원통계/대학원통계.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+library(dplyr)
+
+df <- readxl::read_excel("대학원_20230831213041.xlsx")
+
+seoulGraute <- df %>% 
+  rename(
+    구분_1 = `구분별(1)`,
+    구분_2 = `구분별(2)`,
+    수 = 데이터
+  )
+
+usethis::use_data(seoulGraute, overwrite = TRUE)
+
+
